b1f4dca8fe22b05305ada429b588e282e9c34ec7
Terry Chia <terrycwk1994@gmail.com>
2020-07-14 21:22:18 +0800

    Add option to configure listening port.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
+use clap::{App, Arg};
 use warp::Filter;
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
+    let matches = App::new(clap::crate_name!())
+        .author(clap::crate_authors!())
+        .version(clap::crate_version!())
+        .about(clap::crate_description!())
+        .arg(Arg::with_name("PORT")
+             .short("p")
+             .long("port")
+             .help("Listen on specified port.")
+             .takes_value(true))
+    .get_matches();
+
+    let port = match matches.value_of("PORT") {
+        Some(p) => p.parse().expect("unable to parse PORT"),
+        None => 1337,
+    };
+
     let hello = warp::path!("whatsmyip")
         .and(warp::filters::addr::remote())
         .and_then(|addr: Option<SocketAddr>| async move {
@@ -13,6 +30,6 @@ async fn main() {
         });
 
     warp::serve(hello)
-        .run(([0, 0, 0, 0], 1337))
+        .run(([0, 0, 0, 0], port))
         .await;
 }
